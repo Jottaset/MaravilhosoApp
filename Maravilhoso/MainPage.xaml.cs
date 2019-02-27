e0726f9f534eb67050c08a5d92a274e4d2bacadf
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-02-26 20:29:07 -0400

    first Commit

---
+++ Maravilhoso/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Maravilhoso
+{
+    public partial class MainPage : ContentPage
+    {
+        int i = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void AcaoDeClicar(Object sender, EventArgs e)
+        {
+            string c = i + " Clicadas";
+
+            if (i % 2 != 0)
+            {
+                label_1.Text = "Foi";
+                Button_1.Text = "Clicou";
+                label_2.Text = c;
+            }
+            if (i % 2 == 0)
+            {
+                label_1.Text = "Voltou";
+                Button_1.Text = "Clicastes";
+                label_2.Text = c;
+            }
+            i++;
+        }
+    }
+}
